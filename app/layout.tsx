92b885c0fb99579b685ab909b04ee84f0623b08d
tikitify <tikitify@gmail.com>
2026-06-16 23:45:46 +0200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -53,6 +54,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N5RZP3QJE4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N5RZP3QJE4');
+          `}
+        </Script>
+      </head>
+
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
